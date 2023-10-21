5cc2284ec7df13c171394d6bd8176853a6624b88
Oumb2021 <baarryoumar@gmail.com>
2023-10-21 00:35:01 -0400

    file reading

--- Doubly_Linked_List.c
+++ Doubly_Linked_List.c
@@ -1,5 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
 struct node {
   int data;
   struct node *next;
   struct node *prev;
 };
+
+void insertAtBeginning (int data) {
+
+}
+
+void insertAtEnd (int data){
+}
+
+void insertAfter(int data, int previous){
+
+}
+
+int main (){
+
+}
