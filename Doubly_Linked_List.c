5a9d2515fba9ff1f4b391ceae5634c866a70ae15
Oumb2021 <baarryoumar@gmail.com>
2023-10-22 22:26:17 -0400

    read from file successful

--- Doubly_Linked_List.c
+++ Doubly_Linked_List.c
@@ -6,24 +6,27 @@
 #include <errno.h>
 #include <stdlib.h>
 
-struct node {
+struct node
+{
   int data;
   struct node *next;
   struct node *prev;
 };
 
-void insertAtBeginning (int data) {
-
+void insertAtBeginning(int data)
+{
 }
 
-void insertAtEnd (int data){
+void insertAtEnd(int data)
+{
 }
 
-void insertAfter(int data, int previous){
-
+void insertAfter(int data, int previous)
+{
 }
 
-void displayMenu(){
+void displayMenu()
+{
   printf("---------------------------------------------\n");
   printf("\t\t\t\t DOUBLY LINKED LIST MENU\n");
   printf("---------------------------------------------\n");
@@ -33,23 +36,37 @@ void displayMenu(){
   printf("2. Insert a new node after a given node\n");
   printf("3. Delete a node\n");
   printf("0. Exit program\n");
-
 }
 
-int main() {
+int main()
+{
+
   int ans; // to store the answer of the user
+  FILE *file = fopen("nodeData.txt", "r");
 
-  do {
-    displayMenu();
-    printf("Enter your choice: ");
-    scanf("%d", &ans);
+  if (file == NULL)
+  {
+    perror("Error opening the file");
+    return 1;
+  }
 
-    // Input validation
-    if (ans < 0 || ans > 3) {
-      printf("Please make a valid choice from 0 to 3.\n");
+  int valueRead;
+  while (fscanf(file, "%d", &valueRead) == 1)
+  {
     }
 
-  } while (ans != 0);
+  // while ()
+  // do {
+  //   displayMenu();
+  //   printf("Enter your choice: ");
+  //   scanf("%d", &ans);
+
+  //   // Input validation
+  //   if (ans < 0 || ans > 3) {
+  //     printf("Please make a valid choice from 0 to 3.\n");
+  //   }
+
+  // } while (ans != 0);
 
   return 0;
 }
